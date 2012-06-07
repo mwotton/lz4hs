692f827169b652bfd99b202d943e7d8f52800829
Mark Wotton <mwotton@gmail.com>
2012-06-07 11:18:49 +1000

    fix embedded null bug

--- src/Codec/Compression/LZ4.hs
+++ src/Codec/Compression/LZ4.hs
@@ -3,19 +3,22 @@ import Codec.Compression.LZ4.Foreign
 import Foreign
 import Data.ByteString
 import Debug.Trace
+import Data.ByteString.Unsafe
 
 -- Following zlib's lead, we pretend to be pure on Bytestrings
 -- errors may be thrown for bad formats. I'm not entirely comfortable with this.
 
 compress :: ByteString -> ByteString
-compress x = unsafePerformIO $ useAsCStringLen x $ \(input,len) ->
+compress x = unsafePerformIO $ unsafeUseAsCStringLen x $ \(input,len) ->
   let outLen = c_LZ4_compressBound len in
   allocaBytes (1+outLen) $ \out -> do    
-    written <- c_LZ4_compress input out (fromIntegral outLen)
-    packCStringLen (out, written)
+    written <- c_LZ4_compress input out (fromIntegral len)
+    if written == 0
+      then error "compression failed"
+      else packCStringLen (out, written) --  `doTrace` ("compress", x, len, outLen, written)
 
 uncompress :: ByteString -> ByteString
-uncompress x = unsafePerformIO $ useAsCStringLen x $ \(input,len) ->
+uncompress x = unsafePerformIO $ unsafeUseAsCStringLen x $ \(input,len) ->
   -- this is a complete punt. let's say we use 5*len for the moment,
   -- and extend to lazy bytestrings later.
   let outLen = 5 * len in
@@ -23,4 +26,6 @@ uncompress x = unsafePerformIO $ useAsCStringLen x $ \(input,len) ->
     written <- c_LZ4_uncompress_unknownOutputSize input out (fromIntegral len) (fromIntegral outLen)
     if written < 0
       then error "decompression failed!"
-      else packCString out
+      else packCStringLen (out, written) -- `doTrace` ("uncompress", x, len, written)
+           
+-- doTrace x y = trace (show y) x    
--- test/LZ4Spec.hs
+++ test/LZ4Spec.hs
@@ -1,35 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module LZ4Spec(spec) where
--- import Test.Hspec
+
 import Test.Hspec.QuickCheck
 import Test.Hspec.ShouldBe
 import Control.Applicative
 import Data.ByteString(ByteString,pack)
 import Test.QuickCheck hiding (property)
--- import qualified Test.Hspec.Monadic
 import Codec.Compression.LZ4
 
 
 
 spec :: Specs
-spec = do
-  describe "reverse" $ do
-    it "can compress" $ do
-      -- print (compress $ pack [1])
-      -- print (compress $ pack [1])
+spec = describe "LZ4" $ do
+    it "can compress" $ 
       compress (pack [1]) `shouldBe` compress (pack [1])
-    it "can compress nulls" $ do
-      (uncompress $ compress $ pack [0]) `shouldBe` pack [0]
-    it "can round trip simply" $ do
-      (uncompress $ compress (pack [1..10])) `shouldBe` pack [1..10]
-    it "can roundtrip" $ do
+    it "can compress nulls" $
+      uncompress (compress $ pack [1,0,1]) `shouldBe` pack [1,0,1]
+    it "can round trip simply" $ 
+      uncompress (compress (pack [1..10])) `shouldBe` pack [1..10]
+    it "can roundtrip"
       (property $ forAll string $
-       \s -> (uncompress $ compress s) == s)
+       \s -> uncompress (compress s) == s)
        
 -- FIX really want a better random string algo
 string :: Gen ByteString
 string = do
-   nums <- elements [1..10]
+   nums <- elements [1..10000]
    -- can't handle embedded nulls
-   pack <$> vectorOf nums (elements $ map fromIntegral [1..127])
+   pack <$> vectorOf nums (elements $ map fromIntegral [0..127])
