698b83dd4725593ac97e1602d5ea002bfd75cc9d
Mark Wotton <mwotton@gmail.com>
2013-01-04 13:28:36 +1100

    add regression

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -1,8 +1,11 @@
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE ViewPatterns, OverloadedStrings #-}
 module Main (main) where
 import Test.Hspec
 import Test.Hspec.QuickCheck
+import Test.Hspec.HUnit
+import Test.HUnit
 import Test.QuickCheck
+import Control.Applicative
 
 import qualified Data.ByteString as S
 import Codec.Compression.LZ4
@@ -22,6 +25,13 @@ main = hspec $ do
     prop "is pure (normal)"     $ prop_decompress_pure compress decompress
     prop "is pure (high)"       $ prop_decompress_pure compressHC decompress
     prop "is pure (ultra)"      $ prop_decompress_pure compressPlusHC decompressPlusHC
+  describe "regression test" $ do
+    let input = "\STXd\STX\SOH\NUL\NUL\NUL\NUL\NUL\NUL\NUL\vexample.com\SOH\NUL\NUL\NUL\NUL\NUL\NUL\NUL\ETX\NUL\NUL\NUL\NUL\NUL\NUL\NUL\SI\NUL\NUL\NUL\NUL\NUL\NUL\NUL\tWhirlpool\NUL\NUL\NUL\NUL\NUL\NUL\NUL\vexample.com\NUL\STXf\SOH\SOH\NUL\NUL\NUL\NUL\NUL\NUL\NUL\ffacebook.com\SOH\NUL\NUL\NUL\NUL\NUL\NUL\NUL\SOH\NUL\NUL\NUL\NUL\NUL\NUL\NUL\b\NUL\NUL\NUL\NUL\NUL\NUL\NUL\EOTSHA1\NUL\NUL\NUL\NUL\NUL\NUL\NUL\ffacebook.com\NUL\SOH\NUL\NUL\NUL\NUL\NUL\NUL\NUL\tgmail.com\SOH\NUL\NUL\NUL\NUL\NUL\NUL"
+    it "can compress an oddly full-of-NULLs string"
+      (TestCase $ assertEqual "odd string" (Just input) (return input >>=
+                                                         compress >>= 
+                                                         decompress))
+      
 
 prop_compress_pure comp decomp (S.pack -> xs) =
   (comp xs) == (comp xs)
